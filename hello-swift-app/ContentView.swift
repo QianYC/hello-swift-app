032e388c4f807043bae7f4d7151d3046c62dabf0
macos VM <yc_qian@qq.com>
2020-11-19 06:14:08 -0800

    init project

---
+++ hello-swift-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello-swift-app
+//
+//  Created by qyc on 2020/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hello-swift-app/hello_swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_swift_appApp.swift
+//  hello-swift-app
+//
+//  Created by qyc on 2020/11/19.
+//
+
+import SwiftUI
+
+@main
+struct hello_swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
